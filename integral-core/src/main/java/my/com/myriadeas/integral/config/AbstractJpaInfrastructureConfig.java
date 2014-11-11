803b51ed6022a4278bc8071c70ae772874c9083d
smeiyee <smeiyee@hotmail.com>
2014-11-11 14:56:07 +0800

    Added shared jpa configuration

---
+++ integral-core/src/main/java/my/com/myriadeas/integral/config/AbstractJpaInfrastructureConfig.java
@@ -0,0 +1,33 @@
+package my.com.myriadeas.integral.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@Configuration
+public abstract class AbstractJpaInfrastructureConfig implements
+		JpaInfrastructureConfig {
+
+	@Bean
+	public PlatformTransactionManager transactionManager() {
+		JpaTransactionManager txManager = new JpaTransactionManager();
+		txManager.setEntityManagerFactory(entityManagerFactory().getObject());
+		return txManager;
+	}
+
+	@Override
+	@Bean
+	public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+		HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+		vendorAdapter.setDatabase(Database.HSQL);
+		vendorAdapter.setGenerateDdl(true);
+		LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
+		factory.setJpaVendorAdapter(vendorAdapter);
+		factory.setDataSource(dataSource());
+		return factory;
+	}
+}
