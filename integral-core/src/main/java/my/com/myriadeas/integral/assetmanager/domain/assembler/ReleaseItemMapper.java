beba679c4c6d577ebac1331ea5d00ad23f5cee2f
arlina shah <arlinashah@gmail.com>
2014-12-22 12:08:28 +0800

    Clean integral-web2 from old module of assetManagement

--- integral-core/src/main/java/my/com/myriadeas/integral/assetmanager/domain/assembler/ReleaseItemMapper.java
+++
@@ -1,9 +0,0 @@
-package my.com.myriadeas.integral.assetmanager.domain.assembler;
-
-import my.com.myriadeas.integral.assetmanager.domain.http.ReleaseItemResponse;
-
-public interface ReleaseItemMapper {
-	
-	public ReleaseItemResponse convertTo(Long id, boolean isSuccessful, String message);
-
-}
