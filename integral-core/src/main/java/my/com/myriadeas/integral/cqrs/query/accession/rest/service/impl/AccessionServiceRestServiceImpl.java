6b5c194c91ca29f8ba7f6a45ec0341939c033dc5
smeiyee <smeiyee@hotmail.com>
2015-01-22 14:59:18 +0800

    Added rest service for VuFind Added rest services for Vufind to getStatuses and getHolding

--- integral-core/src/main/java/my/com/myriadeas/integral/cqrs/query/accession/rest/service/impl/AccessionServiceRestServiceImpl.java
+++ integral-core/src/main/java/my/com/myriadeas/integral/cqrs/query/accession/rest/service/impl/AccessionServiceRestServiceImpl.java
@@ -2,7 +2,9 @@ package my.com.myriadeas.integral.cqrs.query.accession.rest.service.impl;
 
 import java.util.List;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -29,6 +31,22 @@ public class AccessionServiceRestServiceImpl {
 	@Path("/query")
 	@Produces("application/json;charset=UTF-8")
 	public Response listAccessionsByRDIds(@QueryParam("rdId") List<String> rdId) {
+		return null;
+	}
+
+	@GET
+	@Path("/{bibliographicId}/holding")
+	@Produces("application/json")
+	public Response getHolding(
+			@PathParam("bibliographicId") String bibliographicId) {
+		return null;
+	}
+
+	@POST
+	@Path("/getStatuses")
+	@Consumes("application/json")
+	@Produces("application/json")
+	public Response getStatuses(List<String> bibliographicIds) {
 		return null;
 	}
 
